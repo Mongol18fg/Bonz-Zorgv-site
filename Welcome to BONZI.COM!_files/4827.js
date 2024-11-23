51eb8c4c81290b40fc0edfc69b3c1436e07b64c9
Mongol18fg <168813714+Mongol18fg@users.noreply.github.com>
2024-11-23 16:04:12 +0700

    Add files via upload

---
+++ Welcome to BONZI.COM!_files/4827.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  //let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+document.writeln('<img src=http://merchant.linksynergy.com/fs/logo/lg_2613.gif>');
+
+
+}
+/*
+     FILE ARCHIVED ON 13:19:52 Apr 06, 2013 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 11:22:21 Nov 10, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 2.448
+  exclusion.robots: 0.025
+  exclusion.robots.policy: 0.015
+  esindex: 0.01
+  cdx.remote: 8.634
+  LoadShardBlock: 305.472 (3)
+  PetaboxLoader3.datanode: 291.225 (4)
+  PetaboxLoader3.resolve: 159.008 (2)
+  load_resource: 305.464
+*/
